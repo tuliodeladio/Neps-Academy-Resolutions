00a6e94f3bb3dcc74c15a95f991442d5f23d6c9f
Deládio <89712759+tuliodeladio@users.noreply.github.com>
2025-03-06 20:21:52 -0300

    Create Organizando Livros.cpp

---
+++ Médio/Organizando Livros.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <map>
+
+
+using namespace std;
+
+int main()
+{   
+    string livros;
+    cin >> livros;
+
+    for (size_t i = 0; i < livros.length(); i++)
+    {
+        for (size_t j = 0; j < livros.length(); j++)
+        {
+            if (livros[i] )
+        }
+    }
+    
+
+
+
+    return 0;
+}
